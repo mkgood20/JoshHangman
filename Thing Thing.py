7708afae62532b6bf4c85393dfd7ea0eda06355d
Bran Kan <60975355+joshf1206@users.noreply.github.com>
2020-02-13 10:33:07 -0500

    Add files via upload
    Initial File

---
+++ Thing Thing.py
@@ -0,0 +1,31 @@
+import random
+randomWords = ["ducks" , "jumbo" , "lucky" , "pills" , "flour"]
+secret = random.choice (randomWords)
+letter = ""
+updateWord = []
+print secret #only for tester
+def initialize():
+    print "We have a secret word"
+    print "_ _ _ _ _"
+def getLetter():
+    print "Enter a letter"
+    global letter
+    letter = raw_input()
+def ifWon():
+    if secret == updateWord:
+        print "you win"
+    else:
+        getLetter()
+def main():
+    initialize ()
+    getLetter ()
+    ifWon ()
+main()
+
+#def tester():
+    #global updateWord
+    #updateWord = raw_input()
+    #if updateWord == secret:
+        #print "yay"
+    #else:
+        #print "no"
